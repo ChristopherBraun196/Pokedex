4b7acadc585d6a52aad077726248fd7b2a8350ed
Chrito196 <Christopher-Braun@outlook.com>
2026-01-11 13:11:40 +0100

    style optimate

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,20 @@
 function PokedexCard(pokemon) {
   return `
-     <div class="pokeCards" tabindex="0" role="button" aria-label="Open ${pokemon.name}">
+     <div onclick=openPokemonDialog() class="pokeCards" tabindex="0" role="button" aria-label="Open ${
+       pokemon.name
+     }">
         <img src="${pokemon.artwork}"  alt="${pokemon.name}">        
         <p class="PositionNumber">#${pokemon.id}</p>
         <h2> ${pokemon.name}</h2>
         <div class="PokeElement">
-        <span class="type ${pokemon.types[0].type.name}">${pokemon.types[0].type.name}</span> 
-        ${pokemon.types[1] ? `<span class="type ${pokemon.types[1].type.name}">${pokemon.types[1].type.name}</span>` : ""}  
+        <span class="type ${pokemon.types[0].type.name}">${
+    pokemon.types[0].type.name
+  }</span> 
+        ${
+          pokemon.types[1]
+            ? `<span class="type ${pokemon.types[1].type.name}">${pokemon.types[1].type.name}</span>`
+            : ""
+        }  
         </div>     
     </div>
     `;
