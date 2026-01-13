e84ccf2a42199369ae009bfb76fcef2bf54ab5fb
Chrito196 <Christopher-Braun@outlook.com>
2026-01-13 19:59:45 +0100

    #

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,8 +2,8 @@ function PokedexCard(pokemon) {
   // console.log(pokemon.types[0].type.name);
   return `
      <div onclick="openPokemonDialog(${pokemon.id})"class="pokeCards ${
-       pokemon.types[0].type.name
-     }"tabindex="0" role="button" aria-label="Open ${pokemon.name}">
+    pokemon.types[0].type.name
+  }"tabindex="0" role="button" aria-label="Open ${pokemon.name}">
         <img src="${pokemon.artwork}"  alt="${pokemon.name}">        
         <p class="PositionNumber">#${pokemon.id}</p>
         <h2> ${pokemon.name}</h2>
@@ -21,23 +21,29 @@ function PokedexCard(pokemon) {
     `;
 }
 
-function BigPokedexCard(pokemon){
-return `
+function BigPokedexCard(pokemon) {
+  return `
     <div class="bigCardContainer">
         <div class="bigCardHeader ${pokemon.types[0].type.name}">
             <button class="closeBtn">X</button>
             <div>
              <p>#${pokemon.id}</p>
-              <h2>${pokemon.name}</h2>                
+              <h3>${pokemon.name}</h3>                
             </div>
             <img class="bigPokemonImage" src="${pokemon.artwork}" alt="${pokemon.name}">
         </div>
 
         <div class="bigCardBody">
             <div class="pokemonStats">
-                <p><strong>HP:</strong> ${pokemon.stats[0].base_stat}</p>
-                <p><strong>Attack:</strong> ${pokemon.stats[1].base_stat}</p>
-                <p><strong>Defense:</strong> ${pokemon.stats[2].base_stat}</p>
+            <h4>Base Stats</h4>
+              <p><strong>HP:</strong> ${pokemon.stats[0].base_stat}</p>
+              <p><strong>Attack:</strong> ${pokemon.stats[1].base_stat}</p>
+              <p><strong>Defense:</strong> ${pokemon.stats[2].base_stat}</p>
+              <p><strong>Sp.Attack:</strong>${pokemon.stats[3].base_stat}</p>
+              <p><strong>Sp.Defense:</strong>${pokemon.stats[4].base_stat}</p>
+              <p><strong>Speed:</strong>${pokemon.stats[5].base_stat}</p>
+             
+
             </div>
         </div>
     </div>
